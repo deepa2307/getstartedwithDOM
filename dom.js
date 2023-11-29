3c4045ddfa16eeb6a6c0360953c7b75c68b30009
deepa2307 <barmeda2307@gmail.com>
2023-11-29 12:27:09 +0530

    GetElementsbyClassname

--- dom.js
+++ dom.js
@@ -3,6 +3,15 @@ var headerTitle = document.getElementById("header-title");
 // headerTitle.textContent = "hello";
 // headerTitle.innerText = "goodbye";
 
-headerTitle.style.borderBottom = "solid 3px #000";
-headerTitle.style.color = "red";
+// headerTitle.style.borderBottom = "solid 3px #000";
+// headerTitle.style.color = "red";
 headerTitle.style.fontWeight = "bold";
+
+var items = document.getElementsByClassName("list-group-item");
+
+items[3].style.backgroundColor = "green";
+// items.style.font = "red";
+
+for (let i = 0; i < items.length; i++) {
+  items[i].style.color = "blue";
+}
