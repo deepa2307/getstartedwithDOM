1d60d0c1e3615425318babcd4965612861f8773d
deepa2307 <barmeda2307@gmail.com>
2023-11-29 12:02:16 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,8 @@
+var headerTitle = document.getElementById("header-title");
+// console.log(headerTitle);
+// headerTitle.textContent = "hello";
+// headerTitle.innerText = "goodbye";
+
+headerTitle.style.borderBottom = "solid 3px #000";
+headerTitle.style.color = "red";
+headerTitle.style.fontWeight = "bold";
